e2eb13f067e77807130014e829d2e49a240e789a
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-03 17:14:25 +0700

    Обновлены конфиги

--- src/Services/Configs/ServerConfig.hpp
+++ src/Services/Configs/ServerConfig.hpp
@@ -5,22 +5,21 @@
 
 #include <nlohmann/json.hpp>
 
-using json = nlohmann::json;
-
 struct ServerConfig
 {
     std::string LOG_FILE;
 
     std::string TG_BOT_KEY;
     std::string TG_BOT_CONFIG;
+    std::string TG_BOT_RESPONSE_CONFIG;
 
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(ServerConfig, LOG_FILE, TG_BOT_KEY, TG_BOT_CONFIG)
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(ServerConfig, LOG_FILE, TG_BOT_KEY, TG_BOT_CONFIG, TG_BOT_RESPONSE_CONFIG)
 };
 
 inline ServerConfig loadServerConfig(const std::string &config_path)
 {
     std::ifstream config_file(config_path);
-    auto config = json::parse(config_file);
+    auto config = nlohmann::json::parse(config_file);
     config_file.close();
     return config.template get<ServerConfig>();
 }
---
+++ src/Services/Configs/TgBotResponseConfig.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+
+#include <nlohmann/json.hpp>
+// "commands":                 ["/start", "/help", "/online", "/renew", "/update", "/registrate"],
+struct TgBotResponseConfig
+{
+    std::string start_command;
+    std::string help_comand;
+    std::string online_comand_success;
+    std::string online_comand_fatal;
+    std::string renew_command_success;
+    std::string renew_command_already;
+    std::string renew_command_fatal;
+    std::string update;
+    std::string registrate_command;
+    std::string not_admin;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(TgBotResponseConfig, start_command, help_comand, online_comand_success, online_comand_fatal, renew_command_success, renew_command_already, renew_command_fatal, update, registrate_command, not_admin)
+};
+
+inline TgBotResponseConfig loadTgBotResponseConfig(const std::string &config_path)
+{
+    std::ifstream config_file(config_path);
+    auto config = nlohmann::json::parse(config_file);
+    config_file.close();
+    return config.template get<TgBotResponseConfig>();
+}
