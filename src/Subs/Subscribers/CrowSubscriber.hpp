830a3b5cf8f6ec0b0a515cab6956b72e8b048d3a
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-17 00:30:55 +0700

    Debug

--- src/Subs/Subscribers/CrowSubscriber.hpp
+++ src/Subs/Subscribers/CrowSubscriber.hpp
@@ -29,9 +29,13 @@ public:
     void sendMessage(const Message &msg) override
     {
         std::string textToSend = msg.toJson().dump();
+        std::cout << "next send: " << textToSend << '\n';
         for(const auto& [key, value] : users_)
             if(value.registered && (value.type == msg.to || msg.to == ClientType::any))
+            {
+                std::cout << "Sending!\n";
                 key->send_text(textToSend);
+            }
     }
 
     void stop() override
