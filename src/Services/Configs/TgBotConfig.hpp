278b5eebc7caf8bae714cc7fa579ae2c1366f614
Игорь Антипов <igorantivirus@yandex.ru>
2025-07-16 23:14:54 +0700

    Добавлен метод получения имени игрока в игре

--- src/Services/Configs/TgBotConfig.hpp
+++ src/Services/Configs/TgBotConfig.hpp
@@ -14,6 +14,12 @@ struct TgBotConfig
     std::set<std::int64_t> CHATS_ID;
     std::map<std::string, std::string> USERS_NICKS;
 
+    const std::string& getMinecraftName(const std::string& tgName) const
+    {
+        const auto found = USERS_NICKS.find(tgName);
+        return found == USERS_NICKS.end() ? tgName : found->second;
+    }
+
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(TgBotConfig, ADMINS_ID, CHATS_ID, USERS_NICKS)
 };
 
